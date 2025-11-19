0599a7304372711cae79342bb385b88f426de8cc
Passw <Passw_Passw@163.com>
2025-11-19 08:52:23 +0800

    build_scripts/Windows-msys2/build.bat : call "%~dp0build\build.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs"

--- build_scripts/Windows-msys2/build.bat
+++ build_scripts/Windows-msys2/build.bat
@@ -11,13 +11,15 @@ if errorlevel 1 goto error_env
 %cecho% info "Build %SourceName%"
 :: call "%~dp0build\build.bat" 64 release autologin "CONFIG+=rs_use_native_dialogs"
 :: call "%~dp0build\build.bat" mingw64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
-call "%~dp0build\build.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
+:: call "%~dp0build\build.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
+call "%~dp0build\build.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs"
 if errorlevel 1 %cecho% error "Failed to build %SourceName%." & exit /B %ERRORLEVEL%
 
 %cecho% info "Pack %SourceName%"
 :: call "%~dp0build\pack.bat" 64 release autologin "CONFIG+=rs_use_native_dialogs"
 :: call "%~dp0build\pack.bat" mingw64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
-call "%~dp0build\pack.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
+:: call "%~dp0build\pack.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs" "CONFIG+=no_rs_sam3" "CONFIG+=no_rs_sam3_libsam3"
+call "%~dp0build\pack.bat" ucrt64 qt6 release "CONFIG+=rs_use_native_dialogs"
 if errorlevel 1 %cecho% error "Failed to pack %SourceName%." & exit /B %ERRORLEVEL%
 
 exit /B 0
